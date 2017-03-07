f308dee6ba8ae3f63f5daf46f403f81fd000b75a
Gregory Guidero <gguidero@dataart.com>
2017-03-07 18:06:01 +0100

    add test environment and fully working Backend Router

--- .gulp/Routes/components.js
+++ .gulp/Routes/components.js
@@ -1,1 +1,7 @@
-module.exports = require('konnektrt')();
+var konnektRT = require('konnektrt')();
+
+module.exports = function(req,res,next){
+  if(!req.sessions) req.sessions = {};
+  req.sessions.cms = true;
+  konnektRT(req,res,next);
+};
