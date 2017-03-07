ec9dbe5d1db2f77ded78dd0452407cc9256fe37e
Gregory Guidero <gguidero@dataart.com>
2017-03-07 19:20:00 +0100

    update with working cms and wrapper code

--- KonnektL/KonnektL.js
+++ KonnektL/KonnektL.js
@@ -1,7 +1,26 @@
+/* An Important Global */
+k_Components = {};
+
 define([],function(){
   function CreateKonnektL()
   {
+    var _onLoad = function(){},
+        _onBatchLoad = function(){};
+
+    /* main takes name and loads file, after load it checks html and cms html if applicable */
     function KonnektL(name)
+    {
+
+    }
+
+    /* checks for unregistered components in the html */
+    KonnektL.getUnkowns = function(html)
+    {
+
+    }
+
+    /* loads script tag into html */
+    KonnektL.load = function(name,url)
     {
 
     }
